554638272714111624d2f1751fbc0c4b9305dd99
Kamesh Sampath <kameshsampath@users.noreply.github.com>
2017-08-24 09:16:55 -0400

    Fixing Circuit Breakers

--- src/main/java/com/redhat/developers/osio/microspringboot2/Microspringboot2Application.java
+++ src/main/java/com/redhat/developers/osio/microspringboot2/Microspringboot2Application.java
@@ -2,8 +2,12 @@ package com.redhat.developers.osio.microspringboot2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableCircuitBreaker
 public class Microspringboot2Application {
 
 	public static void main(String[] args) {
